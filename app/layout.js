a2f7812fb977a2be880995fe7aaaab2b617157ab
aizadramli90 <muhammadaizadr@gmail.com>
2025-05-29 06:11:34 +0800

    add footer links

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./fanta.css";
 import Head from "./head";
+import Link from "next/link";
+import Cart from "@/components/Cart";
+import EmailInput from "@/components/EmailInput";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -22,7 +25,36 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head />
-      <body>{children}</body>
+      <body>
+        <div id="portal" />
+        <div id="app">
+          <header>
+            <div className="header-content">
+              <Link href="/">
+                <h1>Zelora</h1>
+              </Link>
+              <h4 className="mid-text">Honest shopping, happy living</h4>
+              <Cart />
+            </div>
+          </header>
+          <main>{children}</main>
+          <div className="hr" />
+          <footer>
+            <div className="email-container">
+              <h5>
+                Be the first to discover new arrivals, exclusive deals, and
+                exciting offers — all in one place
+              </h5>
+              <EmailInput />
+            </div>
+            <div className="link-container">
+              <h3>Aizadramli</h3>
+              <Link></Link>
+            </div>
+            <div className="socials"></div>
+          </footer>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ components/EmailInput.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function EmailInput() {
+  return (
+    <div className="sign-up">
+      <input placeholder="Your email..." />
+      <button className="button-card">Sign Up</button>
+    </div>
+  );
+}
